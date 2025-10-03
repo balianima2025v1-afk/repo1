1a97efc9d728f7edc4d59887bed12cb13ed37992
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 09:39:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-date_default_timezone_set("Asia/Jakarta");
-echo "x";
-echo "<h1 style='font-family:sans-serif;color:#2c3e50;'>🚀 Deploy Berhasil!</h1>";
-echo "<p>File ini otomatis di-deploy dari <b>GitHub Actions</b> ke server.</p>";
-echo "<p>Waktu server sekarang: " . date("Y-m-d H:i:s") . "</p>";
-?>
+<?php echo "CI/CD test " . date("Y-m-d H:i:s"); ?>
